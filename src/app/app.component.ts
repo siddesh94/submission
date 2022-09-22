ffd37d680947c10d3432d684e246df5464108c5e
siddesh kanojia <siddesh.kanojia@digihealth.in>
2022-09-22 23:34:23 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  selectedRoute: any;
+
   title = 'app-submissions';
+
+  changeRoute(route: any) {
+    this.selectedRoute = route;
+  }
+
+  ngOnInit(): void {
+    this.selectedRoute = 'submission';
+
+    // this.triggerFalseClick();/
+  }
 }
---
+++ src/constant/constant.ts
@@ -0,0 +1,49 @@
+export const constants = {
+    "data": {
+        "form": [
+            "Form 1",
+            "Form 2",
+            "Form 3",
+            "Form 4"
+        ],
+        "status": [
+            "Uncomplete",
+            "Low Risk",
+            "Needs Review"
+        ],
+        "submission_data": [
+            {
+                "workflow": "Requires Location",
+                "from": "sidkan326@gmail.com",
+                "to": "2016sidhesh.kanojia@ves.ac.in",
+                "due_date": "9th September",
+                "status": "Uncomplete",
+                "status_css": "uncomplete"
+            },
+            {
+                "workflow": "Requires Location",
+                "to": "sidkan326@gmail.com",
+                "from": "2016sidhesh.kanojia@ves.ac.in",
+                "due_date": "22th January",
+                "status": "Needs Review",
+                "status_css": "needs_review"
+            },
+            {
+                "workflow": "Requires Location",
+                "from": "sidkan326@yahoo.com",
+                "to": "kanojia@orkut.ac.in",
+                "due_date": "20 September",
+                "status": "Uncomplete",
+                "status_css": "uncomplete"
+            },
+            {
+                "workflow": "Requires Location",
+                "to": "sidkan326@gmail.com",
+                "from": "2016sidhesh.kanojia@ves.ac.in",
+                "due_date": "11th July",
+                "status": "Low Risk",
+                "status_css": "low_risk"
+            }
+        ]
+    }
+}
